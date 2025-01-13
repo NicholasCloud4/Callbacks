bc4b7f9bff122bfc68c1083f942bd67574abaaaf
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-01-13 17:06:36 -0500

    completed

--- index.js
+++ index.js
@@ -12,13 +12,35 @@ document.getElementById("new-deck").addEventListener("click", handleClick)
 
 // setTimeout(callback, 2000)
 
+// const people = [
+//     { name: "Jack", hasPet: true },
+//     { name: "Jill", hasPet: false },
+//     { name: "Alice", hasPet: true },
+//     { name: "Bob", hasPet: false },
+// ]
+
+// function gimmeThePets(person) {
+//     return person.hasPet
+// }
+
+// const peopleWithPets = people.filter(gimmeThePets)
+// console.log(peopleWithPets)
+
+
 /**
  * Challenge: 
  * 
- * Part 1: Given the array of objects below, create a new array with the `.filter()` array method that contains only the objects where "hasPet" is true
+ * Write your own `filter` function! Don't worry about adding it to the prototype of arrays or anything.
+ * This function should take 2 parameters:
+ * 1. The array you want to filter through, and
+ * 2. A callback function
  * 
- * 
- * Part 2: Move the anonymous in-line function to its own, named function
+ * Steps for filterArray function logic:
+ * 1. Initialize a new, empty array which will be returned at the end of the `filterArray`s operations (Completed ✅)
+ * 2. Loop through the array passed as the 1st parameter
+ * 3. Inside the loop, call the callback function, passing the individual item you're currently looping over as the argument to your callback function
+ * 4. If the callback function returns `true`, push the current item you're iterating on in the loop to the new array. If it returns `false`, don't push it to the array.
+ * 5. When the loop is over, return the new array
  */
 
 const people = [
@@ -28,9 +50,26 @@ const people = [
     { name: "Bob", hasPet: false },
 ]
 
-function getThePets(item) {
-    return item.hasPet === true
+function filterArray(array, callback) {
+    const resultingArray = []
+    // Write your filtering logic here
+    array.forEach(element => {
+        callback(element)
+        if (callback(element) === true) {
+            resultingArray.push(element)
+        }
+    });
+    return resultingArray
 }
 
-const peopleWithPets = people.filter(getThePets)
+/**
+ * Challenge: Use your filter array method!
+ * Given the above `people` array, return a new array with only people where `hasPet` is true
+ * Note: Remember that your callback function will be given the individual item in the array for a parameter
+ */
+
+const peopleWithPets = filterArray(people, function (person) {
+    return person.hasPet
+})
+
 console.log(peopleWithPets)
