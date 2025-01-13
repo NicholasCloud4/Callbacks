e070e2dc21faf991c808fc3eacad33cc7bd73944
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-01-13 16:21:45 -0500

    looking at filter

--- index.js
+++ index.js
@@ -6,16 +6,31 @@ function handleClick() {
 
 document.getElementById("new-deck").addEventListener("click", handleClick)
 
+// function callback() {
+//     console.log("I finally ran!")
+// }
+
+// setTimeout(callback, 2000)
+
 /**
  * Challenge: 
  * 
- * Part 1: write a `setTimeout` command. Have it wait for 2000 ms before logging "I finally ran!" to the console
+ * Part 1: Given the array of objects below, create a new array with the `.filter()` array method that contains only the objects where "hasPet" is true
+ * 
  * 
  * Part 2: Move the anonymous in-line function to its own, named function
  */
 
-function sonic() {
-    console.log("I finally ran!")
+const people = [
+    { name: "Jack", hasPet: true },
+    { name: "Jill", hasPet: false },
+    { name: "Alice", hasPet: true },
+    { name: "Bob", hasPet: false },
+]
+
+function getThePets(item) {
+    return item.hasPet === true
 }
 
-setTimeout(sonic, 2000)
+const peopleWithPets = people.filter(getThePets)
+console.log(peopleWithPets)
